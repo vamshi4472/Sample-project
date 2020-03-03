e6b7474116bb067cd6a8af0c9593c67871719fd4
vamshi4472 <61725792+vamshi4472@users.noreply.github.com>
2020-03-03 11:49:44 +0530

    Create Test2.java
    qq

---
+++ myproject/Test2.java
@@ -0,0 +1,23 @@
+package myproject;
+
+public class Test2 {
+
+public static void main(String[] args) {
+int x=0;
+while(true){
+x = increment(x);
+
+System.out.println("Value of x is --"+x);
+
+if(x>=10)
+break;
+
+}
+}
+
+public static int increment(int i){
+return i+1;
+}
+
+
+}
