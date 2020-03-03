6391c10a71802938916f02fbcf1c06da0d4d8998
vamshi4472 <61725792+vamshi4472@users.noreply.github.com>
2020-03-03 11:48:38 +0530

    Create PrimeNumbers.java
    aD

---
+++ myproject/PrimeNumbers.java
@@ -0,0 +1,18 @@
+package myproject;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		 (1,20);
+		
+	
+	}
+	private static void  (int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+	private static void PrimeNumbers(1,20){
+		
+	
+
+
