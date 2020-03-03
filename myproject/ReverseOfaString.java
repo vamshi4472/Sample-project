415af4fa77c2f37d8d5b4bb53c53ee3f6aef1b2f
vamshi4472 <61725792+vamshi4472@users.noreply.github.com>
2020-03-03 11:45:37 +0530

    Create ReverseOfaString.java
    go

---
+++ myproject/ReverseOfaString.java
@@ -0,0 +1,20 @@
+package myproject;
+
+public class ReverseOfaString {
+
+	public static Object main(String[] args) {
+ String str = null;
+StringBuffer sb=new StringBuffer(str);
+ //StringBulider str2 =new StringBulider();
+String rev =sb.reverse().toString();
+ System.out.println(rev);
+ Object revstr = null;
+return revstr;
+ 
+ 
+ 
+	}
+
+	
+
+}
