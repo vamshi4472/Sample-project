f8652829942673b90a878eccb59fbdc1a763bc02
vamshi4472 <61725792+vamshi4472@users.noreply.github.com>
2020-03-03 11:49:25 +0530

    Create Test.java
    ASA

---
+++ myproject/Test.java
@@ -0,0 +1,29 @@
+package myproject;
+
+public class Test {
+
+
+public static void main(String[] args) {
+int i=2;
+f1(1);
+
+}
+
+public static void f1(int i)
+{
+f2(i+1);
+}
+
+public static void f2(int i)
+{
+f3(i+2);
+}
+
+public static void f3(int i)
+{
+System.out.println(i+3);
+}
+
+
+
+}
