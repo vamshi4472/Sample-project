5b306ff14acdb569b3b7919c429584e400b8ec26
vamshi4472 <61725792+vamshi4472@users.noreply.github.com>
2020-03-03 11:49:16 +0530

    Create Test3.java
    SA

---
+++ myproject/Test3.java
@@ -0,0 +1,29 @@
+package myproject;
+public class Test3 {
+
+public static boolean makeDecision(int i)
+{ 
+
+
+public static main (String[] args) {
+int i1=2;
+while(makeDecision(i1)){
+
+i1=i1*i1;
+System.out.println(i1);
+}
+
+
+if(i1%3 != 0){
+return true;
+}else{
+return false;
+}
+}
+
+
+} 
+
+
+
+}
