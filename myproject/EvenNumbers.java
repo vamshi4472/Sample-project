6cbdccffbf37cd0ddb0d94af78205d9eb46b971d
vamshi4472 <61725792+vamshi4472@users.noreply.github.com>
2020-03-03 11:48:29 +0530

    Create EvenNumbers.java
    asd

---
+++ myproject/EvenNumbers.java
@@ -0,0 +1,26 @@
+package myproject;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		
+	 sum();
+		
+		
+		int sum=0;
+		
+		for(int i=1; i<=5;i++)
+			sum=sum+i;
+				
+	
+		{
+				System.out.println(sum+ "");
+
+	}
+
+}
+
+	public static void sum() {
+		System.out.println("even from 1 to 10:");
+		
+	}}
