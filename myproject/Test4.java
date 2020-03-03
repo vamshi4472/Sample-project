e571c454162eed6f9f38487ede67116b62ba73a5
vamshi4472 <61725792+vamshi4472@users.noreply.github.com>
2020-03-03 11:49:34 +0530

    Create Test4.java
    aa

---
+++ myproject/Test4.java
@@ -0,0 +1,21 @@
+package myproject;
+
+public class Test4 {
+
+public static void main(String[] args) {
+int a[][] = new int[10][5];
+for(int i=0;i<10;i++){
+
+for(int j=0; j<5; j++){
+a[i][j]=i*j;
+}
+}
+
+System.out.println(a[0][0]);
+System.out.println(a[1][3]);
+System.out.println(a[3][4]);
+}
+
+
+
+}
