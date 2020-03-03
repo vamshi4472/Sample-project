d29c7adaebfdaf77942d99231f44830230edbf55
vamshi4472 <61725792+vamshi4472@users.noreply.github.com>
2020-03-03 11:49:51 +0530

    Create Interface.java
    za

---
+++ myproject/Interface.java
@@ -0,0 +1,32 @@
+package myproject;
+
+public class Interface {
+
+	public static void main(String[] args) {
+		
+		MyInterface obj=new MyInterface() {
+
+			public void add() {
+				System.out.println("Do addition");
+				
+			}
+
+			public void sub() {
+				System.out.println("Do Subtraction");
+				
+			}
+			
+			
+			
+			
+			
+		};
+		
+		//obj.add();
+		//obj.sub();
+		
+			
+		
+	}
+
+}
