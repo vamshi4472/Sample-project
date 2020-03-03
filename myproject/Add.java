427da787f6e48ca8d5c67e2f967d25006798d75d
vamshi4472 <61725792+vamshi4472@users.noreply.github.com>
2020-03-03 11:44:34 +0530

    Create Add.java
    commit

---
+++ myproject/Add.java
@@ -0,0 +1,17 @@
+package myproject;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=5;
+		int c =a+b;
+		System.out.println(c);
+	}
+		
+			
+	
+//public static int main (string[] []arg ) {
+	//System.out.println(c);
+	
+}
